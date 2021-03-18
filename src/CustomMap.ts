38b435f90802b5bdd215bbc28c973051958bb97b
tomppk <tomi.kivioja@gmail.com>
2021-03-18 14:49:39 +0200

    refactor and setup interface and classes

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,21 @@
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
 export class CustomMap {
   // Instance of class Map. Property googleMap cannot be
   // accessed outside of this class
   private googleMap: google.maps.Map;
 
+  // Takes as argument HTML <div> element id where to render
+  // map. Creating a new Map instance first argument <div>,
+  // second config object to set some options to map
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
@@ -10,6 +23,29 @@ export class CustomMap {
         lat: 0,
         lng: 0,
       },
+    });
+  }
+
+  // Adds marker to the location of a mappable.
+  // Adds event listener when clicked creates and displays
+  // info window. open() first argument reference to our map
+  // instance and second argument our marker where we want
+  // to show info window
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
--- src/User.ts
+++ src/User.ts
@@ -5,8 +5,14 @@
 // Can access faker index.d.ts type definition file by holding
 // down ctrl and clicking faker below
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class User {
+// Our User class implements Mappable interface.
+// This tells TypeScript that we want every User that we
+// create to satisfy the requirements of Mappable interface.
+// This enables TypeScript to notify with error if the
+// interface requirements are not met.
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
@@ -19,5 +25,9 @@ export class User {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
